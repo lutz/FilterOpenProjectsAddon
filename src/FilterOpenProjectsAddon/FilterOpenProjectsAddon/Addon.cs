27e8005158b4047dad9c00b568ef2297fd2a449e
Daniel Lutz <daniel.lutz@citavi.com>
2021-03-10 05:30:42 +0100

    init

---
+++ src/FilterOpenProjectsAddon/FilterOpenProjectsAddon/Addon.cs
@@ -0,0 +1,80 @@
+﻿using FilterOpenProjectsAddon.Properties;
+using SwissAcademic.Citavi.Shell;
+using SwissAcademic.Controls;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FilterOpenProjectsAddon
+{
+    public class Addon : CitaviAddOnEx<OpenProjectDialog>
+    {
+        public override void OnHostingFormLoaded(OpenProjectDialog openProjectDialog)
+        {
+            if (openProjectDialog.GetTabControl() is SwissAcademic.Controls.TabControl tabControl)
+            {
+                openProjectDialog.FormClosed += OpenProjectDialog_FormClosed;
+                tabControl.Size = new Size(tabControl.Size.Width, tabControl.Size.Height - Control2.ScaleY(30));
+                tabControl.SelectedTabChanged += TabControl_SelectedTabChanged;
+
+                var textBox = new TextBox();
+                textBox.Size = new Size(tabControl.Size.Width, textBox.Size.Height);
+                textBox.Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right;
+                textBox.Left = tabControl.Left;
+                textBox.Top = tabControl.Top;
+                textBox.Parent = openProjectDialog;
+                textBox.SetPlaceholder(Resources.TextBox_Placeholder);
+                textBox.TextChanged += TextBox_TextChanged;
+                openProjectDialog.Controls.Add(textBox);
+                tabControl.Location = new Point(tabControl.Left, tabControl.Top + Control2.ScaleY(30));
+
+                openProjectDialog.PerformLayout();
+                textBox.Focus();
+
+
+            }
+        }
+
+        private void TabControl_SelectedTabChanged(object sender, Infragistics.Win.UltraWinTabControl.SelectedTabChangedEventArgs e)
+        {
+            if (sender is SwissAcademic.Controls.TabControl tabControl && tabControl.Parent is OpenProjectDialog openProjectDialog)
+            {
+                TextBox_TextChanged(openProjectDialog.GetTextBox(), EventArgs.Empty);
+            }
+        }
+
+        private void OpenProjectDialog_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (sender is OpenProjectDialog openProjectDialog)
+            {
+                openProjectDialog.FormClosed -= OpenProjectDialog_FormClosed;
+                if (openProjectDialog.GetTabControl() is SwissAcademic.Controls.TabControl tabControl)
+                {
+                    tabControl.SelectedTabChanged -= TabControl_SelectedTabChanged;
+                }
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (sender is TextBox textBox && textBox.Parent is OpenProjectDialog openProjectDialog)
+            {
+                foreach (var control in openProjectDialog.GetKnownProjectsControls())
+                {
+                    if (string.IsNullOrEmpty(textBox.Text))
+                    {
+                        control.Show();
+                    }
+                    else if (control.ToString().Contains(textBox.Text, StringComparison.OrdinalIgnoreCase))
+                    {
+                        control.Show();
+                    }
+                    else
+                    {
+                        control.Hide();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/FilterOpenProjectsAddon/FilterOpenProjectsAddon/Core/Extensions.cs
@@ -0,0 +1,41 @@
+﻿using SwissAcademic.Citavi.Shell;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace FilterOpenProjectsAddon
+{
+    internal static class Extensions
+    {
+        public static SwissAcademic.Controls.TabControl GetTabControl(this OpenProjectDialog openProjectDialog)
+        {
+            return openProjectDialog.Controls.OfType<SwissAcademic.Controls.TabControl>().FirstOrDefault();
+        }
+
+        public static TextBox GetTextBox(this OpenProjectDialog openProjectDialog)
+        {
+            return openProjectDialog.Controls.OfType<TextBox>().FirstOrDefault();
+        }
+
+        public static IEnumerable<Control> GetKnownProjectsControls(this OpenProjectDialog openProjectDialog)
+        {
+            var list = openProjectDialog.GetType().GetProperty("SelectedControlCollection", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(openProjectDialog) as IList;
+            return list.Cast<Control>().ToList();
+        }
+
+        public static void SetPlaceholder(this TextBox textBox, string placeHolder)
+        {
+            if (textBox.IsHandleCreated && placeHolder != null)
+            {
+                SendMessage(textBox.Handle, 0x1501, (IntPtr)1, placeHolder);
+            }
+        }
+        // PInvoke
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, string lp);
+    }
+}
